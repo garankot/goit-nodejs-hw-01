d63b4590029b19b2b89f338b5a77d091cd6e702a
Taras Garan'ko <garanko.t@gmail.com>
2022-05-19 16:51:15 +0300

    edit

--- contacts.js
+++ contacts.js
@@ -1,12 +1,15 @@
 const fs = require("fs/promises");
 const path = require("path");
 
-const contactsPath = path.parse("./db/contacts.json");
+const contactsPath = path.join(__dirname, "contacts.json");
 console.log(contactsPath);
+
 // TODO: задокументировать каждую функцию
-function listContacts() {
-  fs.readFile(contacts.json, []);
-}
+const listContacts = async () => {
+  const data = await fs.readFile(contactsPath);
+  const contData = JSON.parse(data);
+  return contData;
+};
 
 function getContactById(contactId) {
   fs.readFile(contactId, [id]);
--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const contacts = require("./contacts");
-
+// const listContacts = require("")
 console.log(contacts);
-
 // module.exports = {};
